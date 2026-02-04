a4f6ed141007cea9d7fae03bab1c5f0d32e5b947
daliselaineiza-svg <dalis.elaineiza@gmail.com>
2026-02-04 19:07:29 +0800

    Dalis_comprog

---
+++ dalis_activity2.py
@@ -0,0 +1,20 @@
+fullname = "Elaine Iza N. Dalis"
+age = 19
+height_meters = 1.66
+employment = False
+
+print("Fullname          :", fullname)
+print(type(fullname))
+print()
+
+print("Age               :", age)
+print(type(age))
+print()
+
+print("Height in meters  :", height_meters)
+print(type(height_meters))
+print()
+
+print("Currently working?", employment)
+print(type(employment))
+print()
